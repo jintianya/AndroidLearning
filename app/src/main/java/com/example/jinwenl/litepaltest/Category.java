7525337293c4dc9232e644e226fcca9ce5c2cf07
jinwen <jinwenseu@163.com>
2017-05-13 22:20:52 +0800

    Use LitePal

---
+++ app/src/main/java/com/example/jinwenl/litepaltest/Category.java
@@ -0,0 +1,35 @@
+package com.example.jinwenl.litepaltest;
+
+/**
+ * Created by jinwenl on 5/13/2017.
+ */
+
+public class Category {
+    private int id;
+    private String categoryName;
+    private int categoryCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+}
